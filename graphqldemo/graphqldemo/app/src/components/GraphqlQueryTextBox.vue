f06d5b4507f2bae7974d7bcc3b4d8aacfba8793c
Gabriel Sussumu Kato <gkato@braspag.com.br>
2018-05-22 19:56:58 -0300

    Added axios

---
+++ graphqldemo/graphqldemo/app/src/components/GraphqlQueryTextBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <textarea id="queryInput" rows="10" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GraphqlQueryTextBox',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
