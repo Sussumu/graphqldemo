45c2018ea7207bbe611b26d879068f0a096d7a50
Gabriel Sussumu Kato <gkato@braspag.com.br>
2018-05-23 20:20:49 -0300

    Added textarea component to query data from our api

--- graphqldemo/graphqldemo/app/src/components/GraphqlQueryTextBox.vue
+++ graphqldemo/graphqldemo/app/src/components/GraphqlQueryTextBox.vue
@@ -1,14 +1,52 @@
 <template>
-  <div>
-    <textarea id="queryInput" rows="10" />
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label for="queryInput">GraphQL query:</label>
+        <textarea id="queryInput" class="form-control" rows="10" v-model="query"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary" v-on:click.prevent="search">Send query!</button>
+    </form>
+    <div class="container">
+      {{queryResponse}}
+    </div>
   </div>
 </template>
 
 <script>
+import Axios from 'axios'
+
 export default {
   name: 'GraphqlQueryTextBox',
-  props: {
-    
+  components: {
+    Axios
+  },
+  data: function () {
+    return {
+      query: "",
+      queryResponse: ""
+    }
+  },
+  methods: {
+    search: function () {
+      Axios.get("http://localhost:63358/api", this.query)
+        .then(function (response) {
+          this.queryResponse = response.data;
+        })
+        .catch(function (error) {
+          this.queryResponse = error;
+        })
+    },
+    initializeInput: function () {
+      this.query = `query {
+  user {
+    name
+  }
+}`
+    }
+  },
+  mounted: function () {
+    this.initializeInput();
   }
 }
 </script>
