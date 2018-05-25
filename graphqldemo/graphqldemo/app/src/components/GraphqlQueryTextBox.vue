89d4f92d48c233945e26a193d11b32ec1b8b1c70
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2018-05-25 18:04:59 -0300

    Properly added boostrap and jquery (yaay)

--- graphqldemo/graphqldemo/app/src/components/GraphqlQueryTextBox.vue
+++ graphqldemo/graphqldemo/app/src/components/GraphqlQueryTextBox.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container">
-    <form>
+  <div class="row">
+    <form class="col-12">
       <div class="form-group">
         <label for="queryInput">GraphQL query:</label>
         <textarea id="queryInput" class="form-control" rows="10" v-model="query"></textarea>
       </div>
       <button type="submit" class="btn btn-primary" v-on:click.prevent="search">Send query!</button>
     </form>
-    <div class="container">
-      {{queryResponse}}
-    </div>
+      <div class="col-12 mt-5">
+        <pre>
+          <code>{{ queryResponse }}</code>
+        </pre>
+      </div>
   </div>
 </template>
 
@@ -53,4 +55,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
